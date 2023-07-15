cb4a44d2c9edc43281f96a76e76faecdc10f99a9
GioMarcolla <gmarcolla@outlook.com>
2023-07-15 12:41:45 -0300

    Quick tweeks

--- stock-updater/app/Models/StockItems.php
+++ stock-updater/app/Models/StockItems.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class StockItems extends Model
-{
+{   
     use HasFactory;
-    public $timestamps = false;
-    // protected $fillable = ['produto', 'cor', 'tamanho', 'deposito', 'data_disponibilidade', 'quantidade'];
+    public $timestamps = false; // remove mandatory timestamp
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+    protected $fillable = ['produto', 'cor', 'tamanho', 'deposito', 'data_disponibilidade', 'quantidade']; // enabled for mass update "new StockItem($request)"
 }
